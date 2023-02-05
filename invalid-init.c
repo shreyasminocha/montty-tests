a267beaee2c8f60e1a10fc8b818440ef9cc4adfe
Shreyas Minocha <sm-git@riseup.net>
2023-02-05 16:21:06 -0600

    Add more cases to invalid-init

--- invalid-init.c
+++ invalid-init.c
@@ -1,15 +1,24 @@
 #include <assert.h>
+#include <hardware.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <terminals.h>
 #include <unistd.h>
 
+// Make sure you check the return value of `InitHardware` btw
+
 int main() {
 	if (InitTerminalDriver() == ERROR)
 		exit(1);
 
-	int res = InitTerminal(100);
-	assert(res == ERROR);
+	int status = InitTerminal(-1);
+	assert(status == ERROR);
+
+	status = InitTerminal(NUM_TERMINALS); // it's supposed to be exclusive
+	assert(status == ERROR);
+
+	status = InitTerminal(100);
+	assert(status == ERROR);
 
 	exit(0);
 }
